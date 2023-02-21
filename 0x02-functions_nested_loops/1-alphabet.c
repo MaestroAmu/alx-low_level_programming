804ea142220995b97e381ea8572fd1098d455013
MaestroAmu <tossysidney001@gmail.com>
2023-02-21 07:28:20 +0300

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabe, in lowercase, followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
